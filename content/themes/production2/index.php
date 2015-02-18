ac7cec0ac8fd9fca0aa287d8b4beb2f072559424
unknown <1490226@H0323-33.etudiantsfxg.profs.cegep-fxg.qc.ca>
2015-02-18 11:58:51 -0500

    debut de la loop des 10 spectacles à venir

--- content/themes/production2/index.php
+++ content/themes/production2/index.php
@@ -34,13 +34,15 @@
 
 	</section>
 
-	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="<?php echo SCRIPTS; ?>/jquery-1.11.1.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<!-- <script type="text/javascript" src="script/jquery.velocity.min.js"></script> -->
 	<script src="js/script.js"></script>
 </body>
 </html>
 =======
+
+<?
 
 	/**
 	 * index.php
@@ -51,7 +53,7 @@
 	 * Time:    9:28 AM
 	 */
 
-	require_once('header.php');
+	// require_once('header.php');
 ?>
 	<!-- début du contenu de la page index.php -->
 
@@ -59,7 +61,47 @@
 
 	<p>kawabunga</p>
 
-	<!-- C'EST ICI QU'ON MET LA VIANDE ;) --> 
+	<!-- C'EST ICI QU'ON MET LA VIANDE ;) -->
+
+
+	<!-- Affichage des 10 spectacles à venir -->
+
+	<div class="spectacles-a-venir-container">
+		<h2>À venir</h2>
+
+		<?php
+			wp_reset_postdata();						
+			$args = array(
+				'posts_per_page'	=> -1,
+				'post_type' 		=> 'spectacle',
+			);
+			$wp_query = new WP_Query($args);
+
+			if(have_posts())
+			{
+				while(have_posts())
+				{
+					the_post();
+		?>
+					<div class="postContainer">
+						<a href="<?php the_permalink(); ?>">
+							<h3><?php the_title(); ?></h3>
+							<?php
+								if(has_post_thumbnail())
+								{
+									the_post_thumbnail();
+								}
+							?>
+							<div class="date"><?php the_meta(); ?></div>
+						</a>
+					</div>
+		<?php
+				}
+			}
+			else echo '<span>Aucun spectacle à afficher</span>';
+		?>
+
+	</div>
 									  
  
 	
