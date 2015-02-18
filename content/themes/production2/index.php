a213a9293668f605ce992ceeea9ea43a9644545a
Chloé Drolet-Tremblay <chloe@Chloe-MacBook-Pro.local>
2015-02-18 13:22:30 -0500

    Mis du texte pas rapport pour visualiser mieux

--- content/themes/production2/index.php
+++ content/themes/production2/index.php
@@ -1,73 +1,31 @@
+<?php get_header(); ?>
 <?php
-
-/**
- * index.php
- * 
- * Project: wp-production2
- * User:    Félix Dion Robidoux
- * Date:    10/02/2015
- * Time:    9:28 AM
- */
+include_once("include/caroussel.php");
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name = "viewport" content = "width = device-width, initial-scale=1">
-	<title></title>
-	<link rel="stylesheet" href="css/bootstrap.min.css" />
-	<link rel="stylesheet" href="css/style.css" />
-</head>
-<body>
-	<header class="container-fluid">
-	  <div class="container"> 
-		<div class="container">
-		</div>
-	  </div> 
-	</header> 
+<!-- Main hero unit for a primary marketing message or call to action -->
+<div class="hero-unit">
+	<h1>Hello, world!</h1>
+	<p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+	<p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+</div>
+<!-- Example row of columns -->
+<div class="row">
+	<div class="span4">
+		<h2>Heading</h2>
+		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		<p><a class="btn" href="#">View details &raquo;</a></p>
+	</div>
+	<div class="span4">
+		<h2>Heading</h2>
+		<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+		<p><a class="btn" href="#">View details &raquo;</a></p>
+	</div>
+	<div class="span4">
+		<h2>Heading</h2>
+		<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+		<p><a class="btn" href="#">View details &raquo;</a></p>
+	</div>
+</div>
+<?php echo get_template_directory_uri(); ?>
 
-	<section>
-
-	<?php
-    include_once("include/caroussel.php");
-    ?>
-
-	</section>
-
-	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-	<script type="text/javascript" src="js/bootstrap.min.js"></script>
-	<!-- <script type="text/javascript" src="script/jquery.velocity.min.js"></script> -->
-	<script src="js/script.js"></script>
-</body>
-</html>
-=======
-
-	/**
-	 * index.php
-	 * 
-	 * Project: wp-production2
-	 * User:    Félix Dion Robidoux
-	 * Date:    10/02/2015
-	 * Time:    9:28 AM
-	 */
-
-	require_once('header.php');
-?>
-	<!-- début du contenu de la page index.php -->
-
-	
-
-	<p>kawabunga</p>
-
-	<!-- C'EST ICI QU'ON MET LA VIANDE ;) --> 
-									  
- 
-	
-
-
-	<!-- fin du contenu de la page index.php -->
-
-<?php
-	require_once('footer.php');
-?>
->>>>>>> origin/jm-theme-ajout_du_header
+<?php get_footer(); ?>
