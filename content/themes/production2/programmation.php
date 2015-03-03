d4c81340311fb8fe0fa0846d4c98e1275ae7c850
unknown <1490226@H0323-33.etudiantsfxg.profs.cegep-fxg.qc.ca>
2015-03-03 12:13:49 -0500

    Filtre par mois affiché à partir du mois courant

--- content/themes/production2/programmation.php
+++ content/themes/production2/programmation.php
@@ -12,46 +12,69 @@ Template Name: Programmation
 <div class="container">
 	<div class="spectacles-a-venir-container">
 		<h2>Programmation</h2>
+		
 
+		<!-- Liste des mois -->
 		<ul>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_mois=01">Janvier</a></li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_mois=02">Février</a></li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_mois=03">Mars</a></li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_mois=04">Avril</a></li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_mois=05">Mai</a></li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_mois=06">Juin</a></li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_mois=07">Juillet</a></li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_mois=08">Août</a></li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_mois=09">Septembre</a></li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_mois=10">Octobre</a></li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_mois=11">Novembre</a></li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_mois=12">Décembre</a></li>
+			<?php
+
+				// Set local français-canada et timezone
+				setlocale(LC_ALL, 'frc', 'fr_CA');
+				date_default_timezone_set ( 'America/Montreal' );
+
+				// Récupération de la date courante
+				$currentDate = date_create();
+				$currentDate = date_timestamp_get($currentDate);
+
+				// Récupération du mois de la date courante sous le format "01";
+				$moisCourantValeur = strftime('%m', $currentDate);
+
+				// Tableau des mois en français
+				$moisFrancaisTab = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+
+				
+				// Loop qui affiche tous les mois à partir du mois de la date courante
+				for($i=0; $i<12; $i++){
+
+					// Récupération et transformation en integer de la valeur du mois courant afin de correspondre à l'index du tableau
+					$indexTabMoisFrancais = intval($moisCourantValeur-1);
+
+					// Affichage en html
+					echo '<li>
+							<a href="' . get_bloginfo('url') . '/programmation?selection_mois=' . $moisCourantValeur . '">' . $moisFrancaisTab[$indexTabMoisFrancais] . '</a>
+						</li>';
+
+
+					// Incrémentation de $moisCourantValeur
+					$moisCourantValeur++;
+
+					// Si la valeur de $moisCourantValeur dépasse 12, retour à 01
+					if($moisCourantValeur > 12) $moisCourantValeur=1;
+
+					// Formatage de $moisCourantValeur sous le format "01"
+					if(strlen($moisCourantValeur) < 2) $moisCourantValeur='0'.$moisCourantValeur;
+
+				}
+			?>			
 		</ul>
 
+
+		<!-- Liste des catégories -->
 		<ul>
-		<?php
-			$args = array(
-				'orderby' => 'name',
-				'order' => 'ASC'
-			);
+			<?php
+				$args = array(
+					'orderby' => 'name',
+					'order' => 'ASC'
+				);
 
-			$categories = get_categories($args);
+				$categories = get_categories($args);
 
-			foreach($categories as $category) { 
-		    	echo '<li>
-						<a href="' . get_bloginfo('url') . '/programmation?selection_categorie=' . $category->name . '">' . $category->name . '</a>
-					</li>';
-		    }
-		?>
-
-
-		<!-- <ul>
-		
-			<li>
-				<a href="<?php bloginfo('url') ?>/programmation?selection_categorie=<?php echo $category->name ?>"><?php echo $category->name ?></a>
-			</li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_categorie=folk">Folk</a></li>
-			<li><a href="<?php bloginfo('url') ?>/programmation?selection_categorie=alternatif">Alternatif</a></li> -->
+				foreach($categories as $category) { 
+			    	echo '<li>
+							<a href="' . get_bloginfo('url') . '/programmation?selection_categorie=' . $category->name . '">' . $category->name . '</a>
+						</li>';
+			    }
+			?>
 		</ul>
 
 		
