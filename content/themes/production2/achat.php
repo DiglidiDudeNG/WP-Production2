4eaa24d24807549cc75f8b02054f56039ae5c0d7
Chloé Drolet-Tremblay <chloe@Chloe-MacBook-Pro.local>
2015-03-04 12:52:51 -0500

    création d'une page d'achat

---
+++ content/themes/production2/achat.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Achat
+*/
+?>
+<?php get_header(); ?>
+
+	<!-- début du contenu de la page achat.php -->
+
+<div class="container">
+	<div class="achat-container">
+		<h2>Achat</h2>
+	</div>
+</div>
+
+	<!-- fin du contenu de la page achat.php -->
+
+<?php get_footer(); ?>
