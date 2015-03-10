94564f22a329d6f2ae725b107faea2dea959c75a
Chloé Drolet-Tremblay <chloe@Chloe-MacBook-Pro.local>
2015-03-10 09:39:08 -0400

    Améliorer le formulaire d'information client

--- content/themes/production2/include/etape2-form.php
+++ content/themes/production2/include/etape2-form.php
@@ -94,6 +94,7 @@
 			</div>
 			<!-- // -->
 		</div>
+		<!-- fomulaire #2 -->
 		<div class="row">
 			<div class="col-md-6">
 				<!-- PAR COURRIEL -->
@@ -101,73 +102,71 @@
 					<input type="radio" name="envoi" id="parCourriel" value="courriel-envoie"/>
 					<div><span class="bouton-commentaire">Envoyer les billets par courriel</span></div>
 				</label>
-				<!-- COURRIEL -->
-				<div class="form-group">
-					<label for="courriel" class="control-label sr-only">Courriel</label>
-					<div class="input-group">
-						<span class="input-group-addon">Courriel</span>
-						<input class="form-control" type="text" name="courriel" id="courriel" placeholder="Courriel" value="<?php if (isset ($_POST['courriel'])){ echo $courriel;} ?>" required  />
-					</div>
-				</div>
 				<!-- // -->
-				<!-- UTILISER LADRESSE DE courriel ecrit plus haut -->
-				<label for="sameEmail" class="info-condition">
-					<input type="checkbox" name="sameEmail" id="sameEmail" value="sameEmail"/>
-					<div><span class="bouton-commentaire">Utiliser l'adresse courriel plus haut</span></div>
-				</label>
 			</div>
+
 			<div class="col-md-6">
 				<!-- PAR LA POSTE -->
+				<!-- METTRE LA CONDITION DE FAIRE APPARAITRE LE bouton pour adresse differente -->
+
 				<label class="info-condition" for="parPoste">
 					<input type="radio" name="envoi" id="parPoste" value="poste-envoie"/>
 					<div><span class="bouton-commentaire">Envoyer les billets par la poste (+ 2.00$)</span></div>
 				</label>
+
+				<!-- UTILISER LADRESSE PLUS HAUT -->
+				<!-- METTRE LA CONDITION DE FAIRE APPARAITRE LE FORMULAIRE SI LADRESSE EST DIFFÉRENTE DE PLUS HAUT -->
+				<label for="AdresseLivraison" class="info-condition">
+					<input type="checkbox" name="AdresseLivraison" id="AdresseLivraison" value="AdresseLivraison"/>
+					<div><span class="bouton-commentaire">Utiliser une autre adresse de livraison</span></div>
+				</label>
+
 				<!-- NOM -->
 				<div class="form-group">
-					<label for="nom" class="control-label sr-only">Nom</label>
+					<label for="noml" class="control-label sr-only">Nom</label>
 					<div class="input-group">
 						<span class="input-group-addon">Nom</span>
-						<input class="form-control" type="text" name="nom" id="nom" placeholder="Nom" value="<?php if (isset ($_POST['nom'])){ echo $nom;} ?>" required  />
+						<input class="form-control" type="text" name="noml" id="noml" placeholder="Nom" value="<?php if (isset ($_POST['noml'])){ echo $noml;} ?>" required  />
 					</div>
 				</div>
 				<!-- Prénom -->
 				<div class="form-group">
-					<label for="prenom" class="control-label sr-only">Prénom</label>
+					<label for="prenoml" class="control-label sr-only">Prénom</label>
 					<div class="input-group">
 						<span class="input-group-addon">Prénom</span>
-						<input class="form-control" type="text" name="prenom" id="prenom" placeholder="Prénom" value="<?php if (isset ($_POST['prenom'])){ echo $prenom;} ?>" required  />
+						<input class="form-control" type="text" name="prenoml" id="prenoml" placeholder="Prénom" value="<?php if (isset ($_POST['prenoml'])){ echo $prenoml;} ?>" required  />
 					</div>
 				</div>
 				<!-- ADRESSE -->
 				<div class="form-group">
-					<label for="adresse" class="control-label sr-only">Adresse</label>
+					<label for="adressel" class="control-label sr-only">Adresse</label>
 					<div class="input-group">
 						<span class="input-group-addon">Adresse</span>
-						<input class="form-control" type="text" name="adresse" id="adresse" placeholder="Adresse" value="<?php if (isset ($_POST['adresse'])){ echo $adresse;} ?>" required  />
+						<input class="form-control" type="text" name="adressel" id="adressel" placeholder="Adresse" value="<?php if (isset ($_POST['adressel'])){ echo $adressel;} ?>" required  />
 					</div>
 				</div>
 				<!-- Code postal -->
 				<div class="form-group">
-					<label for="codepostal" class="control-label sr-only">Code postal</label>
+					<label for="codepostall" class="control-label sr-only">Code postal</label>
 					<div class="input-group">
 						<span class="input-group-addon">Code postal</span>
-						<input class="form-control" type="text" name="codepostal" id="codepostal" placeholder="Code postal" value="<?php if (isset ($_POST['codepostal'])){ echo $codepostal;} ?>" required  />
+						<input class="form-control" type="text" name="codepostall" id="codepostall" placeholder="Code postal" value="<?php if (isset ($_POST['codepostall'])){ echo $codepostall;} ?>" required  />
 					</div>
 				</div>
 				<!-- Ville -->
 				<div class="form-group">
-					<label for="ville" class="control-label sr-only">Ville</label>
+					<label for="villel" class="control-label sr-only">Ville</label>
 					<div class="input-group">
 						<span class="input-group-addon">Ville</span>
-						<input class="form-control" type="text" name="ville" id="ville" placeholder="Ville" value="<?php if (isset ($_POST['ville'])){ echo $ville;} ?>" required  />
+						<input class="form-control" type="text" name="villel" id="villel" placeholder="Ville" value="<?php if (isset ($_POST['villel'])){ echo $villel;} ?>" required  />
 					</div>
 				</div>
 				<!-- Province -->
 				<div class="form-group">
-					<label for="province" class="control-label sr-only">Province</label>
+					<label for="provincel" class="control-label sr-only">Province</label>
 					<div class="input-group">
 						<span class="input-group-addon">Province</span>
-						<select class="form-control" type="text" name="province" id="province" required >
+						<select class="form-control" type="text" name="provincel" id="provincel" required >
 							<option value="<?php if (isset ($_POST['alberta'])){ echo $alberta;} ?>">Alberta</option>
 							<option value="<?php if (isset ($_POST['cb'])){ echo $cb;} ?>">Colombie-Britanique</option>
 							<option value="<?php if (isset ($_POST['ipe'])){ echo $ipe;} ?>">Ile-du-Prince-Édouard</option>
@@ -185,17 +184,12 @@
 				</div>
 				<!-- Pays -->
 				<div class="form-group">
-					<label for="pays" class="control-label sr-only">Pays</label>
+					<label for="paysl" class="control-label sr-only">Pays</label>
 					<div class="input-group">
 						<span class="input-group-addon">Pays</span>
-						<input class="form-control" type="text" name="pays" id="pays" placeholder="Pays" value="<?php if (isset ($_POST['pays'])){ echo $pays;} ?>" required  />
+						<input class="form-control" type="text" name="paysl" id="paysl" placeholder="Pays" value="<?php if (isset ($_POST['paysl'])){ echo $paysl;} ?>" required  />
 					</div>
 				</div>
-				<!-- UTILISER LADRESSE DE FACTURATION -->
-				<label for="sameAdress" class="info-condition">
-					<input type="checkbox" name="sameAdress" id="sameAdress" value="sameAdress"/>
-					<div><span class="bouton-commentaire">Utiliser l'adresse de facturation</span></div>
-				</label>
 			</div>
 		</div>
 		<input type="hidden" name="etape" id="etape" value="3">
