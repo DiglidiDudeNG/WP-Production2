c6ecc4e1953336cf3ace222755781f7a12d0b053
Chloé Drolet-Tremblay <chloe@Chloe-MacBook-Pro.local>
2015-03-10 10:57:29 -0400

    création de létape 3

--- content/themes/production2/include/etape1-form.php
+++ content/themes/production2/include/etape1-form.php
@@ -1,18 +1,3 @@
-<!-- DONNÉE À PRENDRE -->
-<!-- 	<ul>
-		<li><?php echo $spectacle_titre ?></li>
-		<li><?php echo $spectacle_prix ?>$</li>
-		<li><?php echo $prestation_date ?></li>
-		<li><?php echo $prestation_heure ?></li>
-	</ul>
-	<form action="<?php echo bloginfo('url'); ?>/achat" method="post">
-		<label for="nb_billets">Nombre de billets : </label>
-		<input type="text" name="nb_billets" id="nb_billets" value="2">
-		<input type="hidden" name="etape" id="etape" value="2">
-		<button type="submit" id="vers_etape_2" name="vers_etape_2">Allez à l'étape 2</button>
-	</form>
- -->
-
 <!-- PARTIE CHLOE >>>> -->
 <section class="commande">
 	<!-- ÉTAPE 1 -->
@@ -52,7 +37,7 @@
 							<input type="number" id="nb_billets" name="nb_billets" value="1">
 						</td>
 						<td class="panier-item-total">
-						<!-- METTRE CONDITION DE PRIX PAR RAPPORT AUX NB DE BILLETS -->
+							<!-- METTRE CONDITION DE PRIX PAR RAPPORT AUX NB DE BILLETS -->
 							<?php echo $spectacle_prix ?>$
 							<!-- // -->
 						</td>
@@ -70,19 +55,19 @@
 					<tr class="taxes">
 						<td colspan="6"><strong>TVQ 9.975%</strong></td>
 						<td>
-							<?php 
+							<?php
 								$spectacle_tvq = $spectacle_prix*0.09975;
 								echo round($spectacle_tvq, 2);
-						 	?>$
+							?>$
 						</td>
 					</tr>
 					<tr class="taxes">
 						<td colspan="6"><strong>TPS 5.0%</strong></td>
 						<td>
-							<?php 
+							<?php
 								$spectacle_tps = $spectacle_prix*0.05;
 								echo round($spectacle_tps, 2);
-						 	?>$
+							?>$
 						</td>
 					</tr>
 					<tr class="total">
@@ -90,18 +75,17 @@
 							<strong>Total</strong>
 						</td>
 						<td>
-							<?php 
+							<?php
 								$spectacle_gtotal = $spectacle_prix+$spectacle_tps+$spectacle_tvq;
 								echo round($spectacle_gtotal, 2);
-						 	?>$
+							?>$
 						</td>
 					</tr>
 				</tbody>
 			</table>
 		</div>
 		<!-- Mettre l'adresse de départ du site -->
-		<a class="btn btn-parenthese btn-achat" href="">Annuler la commande</a>
-
+		<a class="btn btn-parenthese btn-achat" href="">Annuler</a>
 		<input type="hidden" name="etape" id="etape" value="2">
 		<input class="btn btn-parenthese btn-achat pull-right" type="submit" value="Étape suivante >">
 	</form>
