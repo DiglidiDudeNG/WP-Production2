831c4b993c616ec4e1a369166cb9954eff106c45
Karine Boisvert <kboisvert3@gmail.com>
2015-03-11 09:53:26 -0400

    change p pour div et ajout span de message d'erreur

--- content/themes/production2/include/etape3-form.php
+++ content/themes/production2/include/etape3-form.php
@@ -10,18 +10,22 @@
 
 		<!-- champs pour entrer les infos de la carte -->
 
-		<p><label for="nomdetenteur"> Nom du détenteur </label>
+		<div><label for="nomdetenteur"> Nom du détenteur </label>
 			<input type="text" name="nom_detenteur" id="nomdetenteur" value="<?php if (isset ($_POST['nomdetenteur'])){ echo $nomdetenteur;} ?>" required  />
-		</p>
-		<p><label for="nocarte"> Numéro de la carte </label>
+		</div>
+		<span class="erreur messageErreurNomdetenteur"><?php echo $messageErreurNomdetenteur; ?></span>
+		<div><label for="nocarte"> Numéro de la carte </label>
 			<input type="text" name="nocarte" id="nocarte" value="<?php if (isset ($_POST['nocarte'])){ echo $nocarte;} ?>" required  />
-		</p>
-				<p><label for="expirationcarte"> Expiration </label>
+		</div>
+		<span class="erreur messageErreurNocarte"><?php echo $messageErreurNocarte; ?></span>
+		<div><label for="expirationcarte"> Expiration </label>
 			<input type="text" name="expirationcarte" id="expirationcarte" value="<?php if (isset ($_POST['expirationcarte'])){ echo $expirationcarte;} ?>" required  />
-		</p>
-				<p><label for="verifcarte"> No. de vérification </label>
+		</div>
+		<span class="erreur messageErreurExpcarte"><?php echo $messageErreurExpcarte; ?></span>
+		<div><label for="verifcarte"> No. de vérification </label>
 			<input type="text" name="verifcarte" id="verifcarte" value="<?php if (isset ($_POST['verifcarte'])){ echo $verifcarte;} ?>" required  />
-		</p>
+		</div>
+		<span class="erreur messageErreurNoverif"><?php echo $messageErreurNoverif; ?></span>
 	</form>
 
 </section>
